b2df4e23f247988d7450971e7c23f60143d22892
MeldwinOfficial <meldwin.mz@gmail.com>
2025-11-10 16:43:14 +0200

    refine more style and prepare to upload site online

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import fs from "fs";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
